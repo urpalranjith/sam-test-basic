d3d9abcff0907874cbc65a70fcce1f5adcdc9e05
Ranjith k <urpalranjith@gmail.com>
2020-09-14 15:05:09 +0530

    48 commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 export const handler = async (event: any = {}): Promise<any> => {
     console.log('Hello World!');
     const response = JSON.stringify(event, null, 2);
-    return response;
+ 
+ return response;
 }
